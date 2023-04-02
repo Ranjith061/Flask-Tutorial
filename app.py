2ce3923998c3094daf50404659a19b335a4d51d9
Ranjith061 <96713888+Ranjith061@users.noreply.github.com>
2023-04-02 08:46:54 +0000

    html and css addition

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def hollo_world():
-  return "Hello Ranjith"
+def home_page():
+  return render_template("home.html")
 
 
 if __name__ == '__main__':
